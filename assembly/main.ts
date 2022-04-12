ecd263c14a0834715c29d063b8bea327b12e296a
Ryan Cloke <ryan@ryancloke.com>
2022-04-12 13:33:43 -0700

    .

--- assembly/main.ts
+++ assembly/main.ts
@@ -1,5 +1,5 @@
-import { Tip, TrackTips, ReturnObject, tips, receivers } from './model';
-import { context, ContractPromiseBatch, PersistentVector, u128 } from "near-sdk-as";
+import { Tip, ReturnObject, ReturnTip, tips, receiverIdx, senderIdx, trackIdx, accountToId, idToAccount, trackOwner } from './model';
+import { context, ContractPromiseBatch, PersistentVector, u128, storage } from "near-sdk-as";
 
 // The maximum number of latest messages the contract returns.
 const TAMA_PC = 3;
@@ -7,86 +7,163 @@ const TAMA_ADDR = "tamago.testnet";
 const MIN_TIP = u128.from('10000000000000000000000');
 const MASTER_ACCOUNT = "backend.tamago.testnet"
 
-export function addTip(trackId: string): ReturnObject<bool> | null {
-  assert(context.attachedDeposit >= MIN_TIP, "Minimum tip is " + MIN_TIP.toString());
-  const amount = context.attachedDeposit;
-  const tip = new Tip(amount);
+export function addTip(trackId: string): ReturnObject<u32> | null {
+
+  if (context.attachedDeposit < MIN_TIP) {
+    return {
+      success: false,
+      error_code: 'MIN_TIP',
+      error_message: "Minimum tip is " + MIN_TIP.toString(),
+      data: 0
+    };
+  }
+
+  const tId = u128.from(trackId).as<u32>();  
+  if (!trackOwner.contains(tId)) {
+    return {
+      success: false,
+      error_code: 'NO_OWNER',
+      error_message: "Track has no owner",
+      data: 0
+    };
+  }  
+
+  const ownerId = trackOwner.getSome(tId);
+  const receiverId = _accountToId(context.sender);
+  const amount = context.attachedDeposit;  
+  const tip = new Tip(tId, ownerId, receiverId, amount);
+
   const rec_amount = u128.mul(u128.div(amount, u128.fromU32(100)), u128.fromU32(TAMA_PC));
-  ContractPromiseBatch.create(_getReceiver(trackId)).transfer(u128.sub(amount, rec_amount));
-  ContractPromiseBatch.create(TAMA_ADDR).transfer(rec_amount);
-  _addTipToTrack(trackId, tip);
-  _addTipToReceiver(_getReceiver(trackId), tip);
+
+  ContractPromiseBatch.create( _idToAccount(u32(ownerId)).toString() ).transfer(u128.sub(amount, rec_amount));
+  ContractPromiseBatch.create( TAMA_ADDR ).transfer(rec_amount);
+
+  let tipInd = storage.getPrimitive<u32>('tc', 0);
+  tipInd++;
+
+  // add to tip map
+  tips.set(tipInd,tip);
+
+  // update indexes
+  let recIdx = receiverIdx.get(u32(ownerId), new Array<u32>());
+  if (recIdx == null) recIdx = new Array<u32>();
+  recIdx.push(tipInd);
+  receiverIdx.set(u32(ownerId),recIdx);
+
+  let senIdx = senderIdx.get(u32(receiverId), new Array<u32>());
+  if (senIdx == null) senIdx = new Array<u32>();
+  senIdx.push(tipInd);
+  senderIdx.set(u32(receiverId),senIdx);
+
+  let traIdx = trackIdx.get(tId, new Array<u32>());
+  if (traIdx == null) traIdx = new Array<u32>();
+  traIdx.push(tipInd);
+  trackIdx.set(tId,traIdx);
+
+
   return {
     success: true,
     error_code: '',
     error_message: '',
-    data: true
+    data: tipInd
   };
+
 }
 
-function _getReceiver(trackId: string): string {
-  let track = tips.get(trackId);
-  assert(track != null, "Impossible to send tip as track hasn't been initialized yet");
-  if (track == null){
-    return "";
+function _accountToId(account: string): u32 {
+  assert(account != '', "Account is blank");
+
+  if (accountToId.contains(account)) {
+    return accountToId.getSome(account);
   }
-  assert(track.receiver != null && track.receiver != "", "Impossible to send tip as receiver isn't set");
-  return track.receiver
+
+  let userCount = storage.getPrimitive<u32>('uc', 0);
+  userCount++;
+  accountToId.set(account, u32(userCount));
+  storage.set<u32>('uc', u32(userCount));
+  return userCount;
+  
 }
 
-function _addTipToTrack(trackId: string, tip: Tip): TrackTips | null{
-  let trackTips = tips.get(trackId);
-  assert(trackTips != null, "Track is undefined");
-  if (trackTips == null){
-    return null;
-  }
-  trackTips.total = u128.add(trackTips.total, tip.amount);
-  trackTips.tips.push(tip);
-  tips.set(trackId, trackTips);
-  return trackTips;
-}
-
-function _addTipToReceiver(receiver: string, tip: Tip): void{
-  let receiverTips = receivers.get(receiver);
-  if (receiverTips == null){
-    receiverTips = new PersistentVector<Tip>(receiver);
-  }
-  receiverTips.push(tip);
-  receivers.set(receiver, receiverTips);
-  return;
+function _idToAccount(id: u32): string {
+  const account = idToAccount.get(id);
+  return account ? account : '';  
 }
 
 export function addTrack(trackId: string, receiver: string): bool {
   assert(context.sender == MASTER_ACCOUNT, "Only " + MASTER_ACCOUNT + " can initialize a track");
-  //assert(tips.get(trackId) == null, "Track has already been initialized");
-  if (tips.get(trackId) == null) {
-    const trackTip = new TrackTips(receiver, trackId);
-    tips.set(trackId, trackTip);
-    return true;
-  } 
-  return false;
+  const tId = u128.from(trackId).as<u32>();
+  trackOwner.set(tId, _accountToId(receiver));
+  return true;
 }
 
-export function getTipsTrack(trackId: string, elements: number=10, offset: number=0): Tip[] | null{
-  const trackTips = tips.get(trackId);
-  //assert(trackTips != null, "Track is undefined");
-  if (trackTips == null){
-    return null;
+export function getTrackOwner(trackId: string): string | null {  
+  const tId = u128.from(trackId).as<u32>();
+  if (trackOwner.contains(tId)) {
+    return _idToAccount(trackOwner.getSome(tId));
   }
+  return null;
+}
+
+export function getTipsTrack(trackId: string): ReturnTip[] | null{
+  const tId = u128.from(trackId).as<u32>();
+  const trackTips = trackIdx.get(tId);
+  if (trackTips != null){
+    let retTips = new Array<ReturnTip>();
+    for (let i = 1; i < trackTips.length; i++) {
+      const t = tips.get(u32(i));
+      if (t != null) retTips[i as u32] = new ReturnTip(t.t, _idToAccount(t.r), _idToAccount(t.s), t.a, t.c);
+    }
+    return retTips;
+  }
+  
+  return new Array<ReturnTip>();  
+}
+
+export function getTipsTrackTotal(trackId: string): ReturnObject<Map<string,u128>>{
+  const tId = u128.from(trackId).as<u32>();
+
+  if (trackIdx.contains(tId)) {
+    const trackTips = trackIdx.getSome(tId);  
+
+    let total = new u128(0);
+    for (let i = 1; i < trackTips.length; i++) {
+      const t = tips.get(i as u32);    
+      if (t) {
+        const amt = t.a;
+        if (amt) {
+          total = u128.add(total, amt);          
+        }
+      }
+    }
+  
+    const rtn = new Map<string,u128>();
+    rtn.set("count",  new u128(trackTips.length));
+    rtn.set("total",  total);
+    return {
+      success: true,
+      error_code:  '',
+      error_message: '',
+      data: rtn
+    };
+  }
+
+  const rtn = new Map<string,u128>();
+  rtn.set("count",  new u128(0));
+  rtn.set("total",  new u128(0));
+  return {
+      success: true,
+      error_code:  '',
+      error_message: '',
+      data: rtn
+  }
+  
+}
+
+/*
+export function getTipsSent(): Tip[] | null {
   let retTips = new Array<Tip>();
-  for (let i = offset * elements; i < (offset * elements + elements) && i < trackTips.tips.length; i++){
-    retTips.push(trackTips.tips[i as i32]);
-  }
-  return retTips;
-}
 
-export function getTipsTrackTotal(trackId: string): u128{
-  const trackTips = tips.get(trackId);
-  //assert(trackTips != null, "Track is undefined");
-  if (trackTips == null){
-    return new u128(0);
-  }
-  return trackTips.total;
 }
 
 export function getTipsReceived(receiverId: string, elements: number=10, offset: number=0): Tip[] | null {
@@ -102,25 +179,32 @@ export function getTipsReceived(receiverId: string, elements: number=10, offset:
   return retTips;
 }
 
-export function getTipsReceivedTotal(receiverId: string): ReturnObject<u128> {
+export function getTipsReceivedTotal(receiverId: string): ReturnObject<Map<string,u128>> {
   const receiverTips = receivers.get(receiverId);
-  //assert(receiverTips != null, "Receiver is undefined");
   if (receiverTips == null){
+    const rtn = new Map<string,u128>();
+    rtn.set("count",  new u128(0));
+    rtn.set("total",  new u128(0));
     return {
-      success: false,
-      error_code:  'RECEIVER_UNDEFINED',
-      error_message: "Receiver is undefined",
-      data: new u128(0)
+      success: true,
+      error_code:  '',
+      error_message: '',
+      data: rtn
     };
   }
+
   let total = new u128(0);
   for (let i = 0; i <= receiverTips.length; i++){
     total = u128.add(total, receiverTips[i].amount);
   }
+  const rtn = new Map<string,u128>();
+  rtn.set("count",  new u128(receiverTips.length));
+  rtn.set("total",  total);
   return {
     success: true,
     error_code: '',
     error_message: '',
-    data: total
+    data: rtn
   };
 }
+*/
--- assembly/model.ts
+++ assembly/model.ts
@@ -1,21 +1,47 @@
 import { context, u128, PersistentVector, PersistentMap } from "near-sdk-as";
 
 const initDate = 1640995200000000000;
-/** 
- * Exporting a new class Comment so it can be used outside of this file.
- */
-@nearBindgen
-export class Tip {
-  sender: string;
-  constructor(public amount: u128) {
-    this.amount = amount;
-    this.sender = context.sender;
-  }
-}
 
 /** 
  * Exporting a new class Comment so it can be used outside of this file.
  */
+@nearBindgen
+export class Tip {  // class written to chain as JSON, hence the short variable names
+  t: u32; // trackId
+  a: u128; // amount
+  r: u32; // receiver id
+  s: u32; // sender id
+  c: u32; // date created
+  constructor(public trackId:u32, receiverId: u32, senderId: u32, amount: u128) {
+    this.t = trackId;
+    this.a = amount;
+    this.r = receiverId;
+    this.s = senderId;
+    this.c = u32((context.blockTimestamp - initDate) / 10 ** 9);
+  }
+}
+
+@nearBindgen
+export class ReturnTip {  // class written to chain as JSON, hence the short variable names
+  trackId: u32; // trackId
+  amount: u128; // amount
+  receiver: string; // receiver id
+  sender: string; // sender id
+  createdAt: u32; // date created
+  constructor(public track:u32, receiver: string, sender: string, amount: u128, created: u32) {
+    this.trackId = track;
+    this.amount = amount;
+    this.receiver = receiver;
+    this.sender = sender;
+    this.createdAt = created;
+  }
+}
+
+
+/** 
+ * Exporting a new class Comment so it can be used outside of this file.
+ */
+/*
  @nearBindgen
  export class TrackTips {
   tips: PersistentVector<Tip>;
@@ -28,14 +54,23 @@ export class Tip {
     this.created = u32((context.blockTimestamp - initDate) / 10 ** 9);
   }
  }
+ */
  
  @nearBindgen
  export class ReturnObject<T>{
-  success: bool;
+  success: bool; 
   error_code: string;
   error_message: string;
   data: T;
 }
+
+export const accountToId = new PersistentMap<string, u32>("ai");
+export const idToAccount = new PersistentMap<u32, string>("ia");
+export const tips        = new PersistentMap<u32, Tip>("t"); // 1 {1, 2, 11} // 2 {2, 2, 11}
+export const receiverIdx = new PersistentMap<u32, Array<u32>>("rx"); // reciever1 [1,2]
+export const senderIdx   = new PersistentMap<u32, Array<u32>>("sx"); // sender1 [1] // sender2 [2]
+export const trackIdx    = new PersistentMap<u32, Array<u32>>("tx"); // 12 (trackid)  [1,2]
+export const trackOwner  = new PersistentMap<u32, u32>("to"); // 1 2 - user 1 owns track 2
 
 /**
  * collections.vector is a persistent collection. Any changes to it will
@@ -43,5 +78,5 @@ export class Tip {
  * The parameter to the constructor needs to be unique across a single contract.
  * It will be used as a prefix to all keys required to store data in the storage.
  */
-export const tips = new PersistentMap<string, TrackTips>("");
-export const receivers = new PersistentMap<string, PersistentVector<Tip>>("r");
+//export const tips = new PersistentMap<string, TrackTips>("");
+//export const receivers = new PersistentMap<string, PersistentVector<Tip>>("r");
